c80141c021bc325f49eb672bd2f5d736ac622e91
Cory Benfield <lukasaoz@gmail.com>
2013-05-04 15:12:48 +0100

    Define basic API.

--- httpcache/cache.py
+++ httpcache/cache.py
@@ -6,10 +6,27 @@ cache.py
 Contains the primary cache structure used in http-cache.
 """
 
+
 class HTTPCache(object):
     """
     The HTTP Cache object. Manages caching of responses according to RFC 2616,
     adding necessary headers to HTTP request objects, and returning cached
     responses based on server responses.
     """
-    pass
+    def __init__(self, capacity=50):
+        self.capacity = capacity
+
+    def store(self, response):
+        """
+        Takes an HTTP response object and stores it in the cache according to
+        RFC 2616.
+        """
+        pass
+
+    def retrieve(self, request):
+        """
+        Retrieves a cached response if possible. If no cached response is
+        immediately available, but it may be possible to re-use an old one,
+        will attach an If-Modified-Since header to the request.
+        """
+        pass
