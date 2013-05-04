6a44a5d76e7e56dae2d5ebdc8260fe806dced74f
Cory Benfield <lukasaoz@gmail.com>
2013-05-04 14:55:15 +0100

    Basic file structure.

---
+++ httpcache/cache.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+cache.py
+~~~~~~~~
+
+Contains the primary cache structure used in http-cache.
+"""
+
+class HTTPCache(object):
+    """
+    The HTTP Cache object. Manages caching of responses according to RFC 2616,
+    adding necessary headers to HTTP request objects, and returning cached
+    responses based on server responses.
+    """
+    pass
